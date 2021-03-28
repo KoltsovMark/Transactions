e4a2939374616f12e14f23a09a8081cf0b526437
koltsov_ml <koltsov_ml@groupbwt.com>
2021-03-28 16:27:05 +0300

    add loading supported currenices on service level

--- src/Service/Currency.php
+++ src/Service/Currency.php
@@ -2,6 +2,8 @@
 
 namespace CommissionTask\Service;
 
+use CommissionTask\Model\Currency as CurrencyModel;
+
 class Currency
 {
     public const EUR = 'EUR';
@@ -9,9 +11,19 @@ class Currency
     public const JPY = 'JPY';
 
     /**
+     * @var CurrencyModel[]
+     */
+    protected array $currencies = [];
+
+    public function __construct()
+    {
+        $this->loadCurrencies();
+    }
+
+    /**
      * @return string[]
      */
-    public static function getSupportedCurrencies(): array
+    protected function getSupportedCurrenciesCodes(): array
     {
         return [
             self::EUR,
@@ -20,8 +32,60 @@ class Currency
         ];
     }
 
+    protected function loadCurrencies(): void
+    {
+        foreach (self::getSupportedCurrenciesCodes() as $currencyCode) {
+            $this->addCurrency($currencyCode);
+        }
+    }
+
+    /**
+     * @param string $currencyCode
+     *
+     * @return CurrencyModel|null
+     */
+    public function getCurrencyByCodeOrNull(string $currencyCode): ?CurrencyModel
+    {
+        foreach ($this->getCurrencies() as $currency) {
+            if ($currency->getCode() === $currencyCode) {
+                return $currency;
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * @param string $currencyCode
+     *
+     * @return $this
+     */
+    public function addCurrency(string $currencyCode): Currency
+    {
+        $currency = $this->getCurrencyByCodeOrNull($currencyCode);
+
+        if (empty($currency)) {
+            $this->currencies[] = new CurrencyModel($currencyCode);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return CurrencyModel[]
+     */
+    public function getCurrencies(): array
+    {
+        return $this->currencies;
+    }
+
+    /**
+     * @param string $currencyCode
+     *
+     * @return bool
+     */
     public function isSupportedCurrency(string $currencyCode): bool
     {
-        return  \in_array($currencyCode, self::getSupportedCurrencies());
+        return (bool) $this->getCurrencyByCodeOrNull($currencyCode);
     }
 }
