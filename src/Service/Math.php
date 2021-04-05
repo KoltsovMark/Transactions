ef93237cbcbb3f9d3882cec70fd7cf05f040449d
koltsov_ml <koltsov_ml@groupbwt.com>
2021-04-05 18:08:48 +0300

    add divide to Math

--- src/Service/Math.php
+++ src/Service/Math.php
@@ -4,17 +4,52 @@ declare(strict_types=1);
 
 namespace CommissionTask\Service;
 
+use Brick\Math\RoundingMode;
+use Brick\Money\Context\CustomContext;
+use Brick\Money\Money;
+
 class Math
 {
-    private $scale;
+    public const ROUNDING_MODE = RoundingMode::UP;
+    protected const DEFAULT_CURRENCY = 'EUR';
 
-    public function __construct(int $scale)
+    private $roundingMode;
+    private $context;
+
+    public function __construct(int $scale = 2, int $roundingMode = self::ROUNDING_MODE)
     {
-        $this->scale = $scale;
+        $this->context = new CustomContext($scale);
+        $this->roundingMode = $roundingMode;
     }
 
+    /**
+     * @param string $leftOperand
+     * @param string $rightOperand
+     *
+     * @return string
+     * @throws \Brick\Money\Exception\MoneyMismatchException
+     * @throws \Brick\Money\Exception\UnknownCurrencyException
+     */
     public function add(string $leftOperand, string $rightOperand): string
     {
-        return bcadd($leftOperand, $rightOperand, $this->scale);
+        $leftOperand = Money::of($leftOperand, self::DEFAULT_CURRENCY, $this->context, $this->roundingMode);
+        $rightOperand = Money::of($rightOperand, self::DEFAULT_CURRENCY, $this->context, $this->roundingMode);
+
+        return (string) $leftOperand->plus($rightOperand, self::ROUNDING_MODE)->getAmount();
+    }
+
+    /**
+     * @param string $leftOperand
+     * @param string $rightOperand
+     *
+     * @return string
+     * @throws \Brick\Money\Exception\UnknownCurrencyException
+     */
+    public function divide(string $leftOperand, string $rightOperand)
+    {
+        $leftOperand = Money::of($leftOperand, self::DEFAULT_CURRENCY, $this->context, $this->roundingMode);
+        $rightOperand = Money::of($rightOperand, self::DEFAULT_CURRENCY, $this->context, $this->roundingMode);
+
+        return (string) $leftOperand->dividedBy($rightOperand, $this->roundingMode)->getAmount();
     }
 }
