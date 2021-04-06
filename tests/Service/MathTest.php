cf36bed2b3c93fa605bd866008c80d122f7d4b94
koltsov_ml <koltsov_ml@groupbwt.com>
2021-04-06 12:56:36 +0300

    add divide tests

--- tests/Service/MathTest.php
+++ tests/Service/MathTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace CommissionTask\Tests\Service;
 
+use Brick\Math\Exception\DivisionByZeroException;
 use PHPUnit\Framework\TestCase;
 use CommissionTask\Service\Math;
 
@@ -13,7 +14,7 @@ class MathTest extends TestCase
 
     public function setUp()
     {
-        $this->math = new Math(2);
+        $this->math = new Math();
     }
 
     /**
@@ -21,16 +22,18 @@ class MathTest extends TestCase
      * @param string $rightOperand
      * @param string $expectation
      *
+     * @covers \CommissionTask\Service\Math::add
+     *
      * @dataProvider dataProviderForAddTesting
      */
     public function testAdd(string $leftOperand, string $rightOperand, string $expectation)
     {
-        $this->assertEquals(
-            $expectation,
-            $this->math->add($leftOperand, $rightOperand)
-        );
+        $this->assertEquals($expectation, $this->math->add($leftOperand, $rightOperand));
     }
 
+    /**
+     * @return \string[][]
+     */
     public function dataProviderForAddTesting(): array
     {
         return [
@@ -38,5 +41,39 @@ class MathTest extends TestCase
             'add negative number to a positive' => ['-1', '2', '1'],
             'add natural number to a float' => ['1', '1.05123', '2.06'],
         ];
+    }
+
+    /**
+     * @param string $leftOperand
+     * @param string $rightOperand
+     * @param string $expectation
+     *
+     * @covers \CommissionTask\Service\Math::divide
+     *
+     * @dataProvider dataProviderForDivideTesting
+     */
+    public function testDivide(string $leftOperand, string $rightOperand, string $expectation)
+    {
+        $this->assertEquals($expectation, $this->math->divide($leftOperand, $rightOperand));
+    }
+
+    public function dataProviderForDivideTesting(): array
+    {
+        return [
+            'divide 2 natural numbers' => ['6', '2', '3'],
+            'divide natural number to a negative float' => ['-10', '2.5358', '-3.95'],
+            'divide natural number to a float' => ['10', '2.5358', '3.95'],
+            'divide negative natural number to a negative natural number' => ['-10', '-2', '5'],
+        ];
+    }
+
+    /**
+     * @covers \CommissionTask\Service\Math::divide
+     */
+    public function testDivideNaturalNumberToZero()
+    {
+        $this->expectException(DivisionByZeroException::class);
+
+        $this->math->divide('10', '0');
     }
 }
