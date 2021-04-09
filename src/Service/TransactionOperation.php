cf7be6f4f6723f9edc2ee61c71e6630398eb27b2
koltsov_ml <koltsov_ml@groupbwt.com>
2021-04-09 10:00:24 +0300

    add TransactionOperationService

---
+++ src/Service/TransactionOperation.php
@@ -0,0 +1,81 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CommissionTask\Service;
+
+use CommissionTask\Dto\NewTransaction as NewTransactionDto;
+use CommissionTask\Exception\FunctionalInDevelopment as FunctionalInDevelopmentException;
+use CommissionTask\Factory\Currency as CurrencyFactory;
+use CommissionTask\Factory\Customer as CustomerFactory;
+use CommissionTask\Factory\Transaction as TransactionFactory;
+use CommissionTask\Factory\TransactionCommission as TransactionCommissionFactory;
+use CommissionTask\Model\Transaction as TransactionModel;
+use CommissionTask\Repository\Transaction as TransactionRepository;
+use CommissionTask\Service\Commission as CommissionService;
+
+class TransactionOperation
+{
+    private TransactionRepository $transactionRepository;
+    private CommissionService $commissionService;
+    private TransactionCommissionFactory $transactionCommissionFactory;
+    private TransactionFactory $transactionFactory;
+    private CustomerFactory $customerFactory;
+    private CurrencyFactory $currencyFactory;
+
+    /**
+     * Transaction constructor.
+     *
+     * @param Commission $commissionService
+     */
+    public function __construct(
+        CommissionService $commissionService,
+        TransactionRepository $transactionRepository,
+        TransactionCommissionFactory $transactionCommissionFactory,
+        TransactionFactory $transactionFactory,
+        CustomerFactory $customerFactory,
+        CurrencyFactory $currencyFactory
+    ) {
+        $this->transactionRepository = $transactionRepository;
+        $this->commissionService = $commissionService;
+        $this->transactionCommissionFactory = $transactionCommissionFactory;
+        $this->transactionFactory = $transactionFactory;
+        $this->customerFactory = $customerFactory;
+        $this->currencyFactory = $currencyFactory;
+    }
+
+    /**
+     * @throws FunctionalInDevelopmentException
+     * @throws \Brick\Money\Exception\UnknownCurrencyException
+     */
+    public function processTransaction(NewTransactionDto $newTransactionDto): TransactionModel
+    {
+        $transactionCommissionDto = $this->transactionCommissionFactory->createFromNewTransactionDto($newTransactionDto);
+        $transaction = $this->transactionFactory->createFromNewTransactionDto($newTransactionDto);
+        $customer = $this->customerFactory->createFromNewTransactionDto($newTransactionDto);
+        $currency = $this->currencyFactory->create($newTransactionDto->getCurrencyCode());
+
+        $transaction->setCustomer($customer);
+        $transaction->setCurrency($currency);
+
+        if ($transaction->isCashIn()) {
+            $commission = $this->commissionService->calculateCashInCommission($transactionCommissionDto);
+        } elseif ($transaction->isCashOut()) {
+            if ($customer->isLegalPerson()) {
+                $commission = $this->commissionService->calculateCashOutLegalCommission($transactionCommissionDto);
+            } elseif ($customer->isNaturalPerson()) {
+                $commission = $this->commissionService->calculateCashOutNaturalCommission($transactionCommissionDto);
+            } else {
+                throw new FunctionalInDevelopmentException();
+            }
+        } else {
+            throw new FunctionalInDevelopmentException();
+        }
+
+        $transaction->setCommission($commission);
+
+        $this->transactionRepository->add($transaction);
+
+        return $transaction;
+    }
+}
---
+++ tests/Service/TransactionOperationTest.php
@@ -0,0 +1,177 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CommissionTask\Tests\Service;
+
+use CommissionTask\Factory\Currency as CurrencyFactory;
+use CommissionTask\Factory\Customer as CustomerFactory;
+use CommissionTask\Factory\NewTransaction as NewTransactionFactory;
+use CommissionTask\Factory\Transaction as TransactionFactory;
+use CommissionTask\Factory\TransactionCommission;
+use CommissionTask\Factory\TransactionCommission as TransactionCommissionFactory;
+use CommissionTask\Repository\Transaction as TransactionRepository;
+use CommissionTask\Service\Commission as CommissionService;
+use CommissionTask\Service\TransactionOperation as TransactionOperationService;
+use PHPUnit\Framework\TestCase;
+use DateTime;
+
+class TransactionOperationTest extends TestCase
+{
+    protected TransactionOperationService $transactionOperationService;
+    protected CommissionService $commissionServiceMock;
+    protected TransactionRepository $transactionRepositoryMock;
+    protected TransactionCommissionFactory $transactionCommissionFactoryMock;
+    protected TransactionFactory $transactionFactoryMock;
+    protected CustomerFactory $customerFactoryMock;
+    protected CurrencyFactory $currencyFactoryMock;
+
+    /**
+     * @covers \CommissionTask\Service\Transaction::processTransaction
+     *
+     * @dataProvider dataProviderForProcessTransaction
+     */
+    public function testProcessTransaction(
+        string $amount,
+        string $currencyCode,
+        int $customerId,
+        string $customerType,
+        string $transactionType,
+        string $createdAt,
+        string $expectedMethod,
+        string $expectedCommission
+    ) {
+        $newTransactionDto = (new NewTransactionFactory())->createFromArray([
+                $createdAt,
+                $customerId,
+                $customerType,
+                $transactionType,
+                $amount,
+                $currencyCode,
+            ]
+        );
+        $transactionCommissionDto = (new TransactionCommission())->createFromNewTransactionDto($newTransactionDto);
+        $transaction = (new TransactionFactory())->createFromNewTransactionDto($newTransactionDto);
+        $customer = (new CustomerFactory())->createFromNewTransactionDto($newTransactionDto);
+        $currency = (new CurrencyFactory())->create($newTransactionDto->getCurrencyCode());
+
+        $this->transactionCommissionFactoryMock
+            ->expects($this->once())
+            ->method('createFromNewTransactionDto')
+            ->with(...[$newTransactionDto])
+            ->willReturn($transactionCommissionDto)
+        ;
+
+        $this->transactionFactoryMock
+            ->expects($this->once())
+            ->method('createFromNewTransactionDto')
+            ->with(...[$newTransactionDto])
+            ->willReturn($transaction)
+        ;
+
+        $this->customerFactoryMock
+            ->expects($this->once())
+            ->method('createFromNewTransactionDto')
+            ->with(...[$newTransactionDto])
+            ->willReturn($customer)
+        ;
+
+        $this->currencyFactoryMock
+            ->expects($this->once())
+            ->method('create')
+            ->with(...[$currencyCode])
+            ->willReturn($currency)
+        ;
+
+        $this->commissionServiceMock
+            ->expects($this->once())
+            ->method($expectedMethod)
+            ->with($transactionCommissionDto)
+            ->willReturn($expectedCommission)
+        ;
+
+        $this->transactionRepositoryMock
+            ->expects($this->once())
+            ->method('add')
+            ->with($transaction)
+        ;
+
+        $newTransaction = $this->transactionOperationService->processTransaction($newTransactionDto);
+
+        $this->assertSame($amount, $newTransaction->getAmount());
+        $this->assertSame($expectedCommission, $newTransaction->getCommission());
+        $this->assertEquals(new DateTime($createdAt), $newTransaction->getCreatedAt());
+        $this->assertSame($currency, $newTransaction->getCurrency());
+        $this->assertSame($customer, $newTransaction->getCustomer());
+        $this->assertSame($transactionType, $newTransaction->getType());
+    }
+
+    /**
+     * @return array[]
+     */
+    public function dataProviderForProcessTransaction()
+    {
+        return [
+            'new cash in transaction of natural customer' => [
+                '15.23',
+                'EUR',
+                2,
+                'natural',
+                'cash_in',
+                '2017-10-21',
+                'calculateCashInCommission',
+                '10.00'
+            ],
+            'new cash in transaction of legal customer' => [
+                '15.23',
+                'EUR',
+                2,
+                'legal',
+                'cash_in',
+                '2017-10-21',
+                'calculateCashInCommission',
+                '10.00'
+            ],
+            'new cash out transaction of natural customer' => [
+                '15.23',
+                'EUR',
+                2,
+                'natural',
+                'cash_out',
+                '2017-10-21',
+                'calculateCashOutNaturalCommission',
+                '10.00'
+            ],
+            'new cash out transaction of legal customer' => [
+                '15.23',
+                'EUR',
+                2,
+                'legal',
+                'cash_out',
+                '2017-10-21',
+                'calculateCashOutLegalCommission',
+                '10.00'
+            ],
+        ];
+    }
+
+    protected function setUp(): void
+    {
+        parent::setUp(); // TODO: Change the autogenerated stub
+
+        $this->transactionRepositoryMock = $this->createMock(TransactionRepository::class);
+        $this->commissionServiceMock = $this->createMock(CommissionService::class);
+        $this->transactionCommissionFactoryMock = $this->createMock(TransactionCommissionFactory::class);
+        $this->transactionFactoryMock = $this->createMock(TransactionFactory::class);
+        $this->customerFactoryMock = $this->createMock(CustomerFactory::class);
+        $this->currencyFactoryMock = $this->createMock(CurrencyFactory::class);
+        $this->transactionOperationService = new TransactionOperationService(
+            $this->commissionServiceMock,
+            $this->transactionRepositoryMock,
+            $this->transactionCommissionFactoryMock,
+            $this->transactionFactoryMock,
+            $this->customerFactoryMock,
+            $this->currencyFactoryMock
+        );
+    }
+}
